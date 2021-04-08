107e6135c1decd9271d41addd25cc1210418af9f
arunijay <arunijay@users.noreply.github.com>
2021-04-08 22:56:23 +1000

    aruni

--- templates/Customers/index.php
+++ templates/Customers/index.php
@@ -5,7 +5,7 @@
  */
 ?>
 <div class="customers index content">
-    <?= $this->Html->link(__('New new Customer'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+    <?= $this->Html->link(__('New new two Customer'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Customers') ?></h3>
     <div class="table-responsive">
         <table>
