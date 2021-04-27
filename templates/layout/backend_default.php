f753ccf048aef4f29db3374e5620cc8b3265aa1e
arunijay <arunijay@users.noreply.github.com>
2021-04-28 01:24:19 +1000

    Aruni Backend CSS

--- templates/layout/backend_default.php
+++ templates/layout/backend_default.php
@@ -7,38 +7,44 @@
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     0.10.0
- * @license   https://opensource.org/licenses/mit-license.php MIT License
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
  * @var \App\View\AppView $this
  */
-use Cake\Cache\Cache;
-use Cake\Core\Configure;
-use Cake\Core\Plugin;
-use Cake\Datasource\ConnectionManager;
-use Cake\Error\Debugger;
-use Cake\Http\Exception\NotFoundException;
 
-$this->disableAutoLayout();
-
-if (!Configure::read('debug')) :
-    throw new NotFoundException(
-        'Please replace templates/Pages/home.php with your own version or re-enable debug mode.'
-    );
-endif;
-$this->Html->css('sb-admin-2', ['block' => true]);
-$this->Html->css('sb-admin-2.min.css', ['block' => true]);
-$this->Html->css(' style', ['block' => true]);
-$this->Html->css('min', ['block' => true]);
+$cakeDescription = 'CakePHP: the rapid development php framework';
+$this->Html->css('my', ['block' => true]);
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?= $cakeDescription ?>:
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
 
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+
+    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
+
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?>
 </head>
 <body>
+<nav class="top-nav">
+    <div class="top-nav-title">
+        <a href="<?= $this->Url->build('/') ?>"><span>Backend</span></a>
+    </div>
+    <div class="top-nav-links">
+        <?= $this->Html->link(__('Log In'), ['action' => '../Users/login'], ['class' => 'button float-right']) ?>
+    </div>
+</nav>
 <main class="main">
     <div class="container">
         <?= $this->Flash->render() ?>
@@ -49,3 +55,24 @@ $this->Html->css('min', ['block' => true]);
 </footer>
 </body>
 </html>
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.10.0
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ * @var \App\View\AppView $this
+ */
+$cakeDescription = "Paul's Honey Administration";
+
+$this->Html->css('sb-admin-2.min', ['block' => true]);
+$this->Html->script('js/sb-admin-2.min.js', ['block'=>true]);
+?>
+
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -7,51 +7,123 @@
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.10.0
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
  * @var \App\View\AppView $this
  */
+$cakeDescription = "Paul's Honey Administration";
 
-$cakeDescription = 'CakePHP: the rapid development php framework';
-$this->Html->css('my', ['block' => true]);
+$this->Html->css('sb-admin-2.min', ['block' => true]);
+$this->Html->script('js/sb-admin-2.min.js', ['block'=>true]);
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <?= $this->Html->charset() ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        <?= $cakeDescription ?>:
-        <?= $this->fetch('title') ?>
-    </title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <?= $this->Html->meta('icon') ?>
-
+    <title><?= $this->fetch('title') ?></title>
+    <?= $this->Html->css('/vendor/fontawesome-free/css/all.min.css')?>
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
+    <?= $this->Html->css('sb-admin-2.min.css') ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
-    <?= $this->fetch('script') ?>
+
+
 </head>
-<body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Home</span></a>
-        </div>
-        <div class="top-nav-links">
-            <?= $this->Html->link(__('Log In'), ['action' => '../Users/login'], ['class' => 'button float-right']) ?>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
-            <?= $this->Flash->render() ?>
-            <?= $this->fetch('content') ?>
-        </div>
-    </main>
-    <footer>
-    </footer>
+<body id="page-top">
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= $this->Url->build('/') ?>">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Paul's Honey Admin</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Pages
+            </div>
+
+            <!-- Nav Item - Products Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="<?= $this->Url->build(['controller'=>'Products','action' => 'index']) ?>" data-toggle="collapse" data-target="#collapseProducts"
+                   aria-expanded="true" aria-controls="collapseProducts">
+                    <span>Products</span>
+                </a>
+                <div id="collapseProducts" class="collapse" aria-labelledby="headingProducts" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Product Options</h6>
+                        <a class="collapse-item" href="<?= $this->Url->build(['controller'=>'Products','action' => 'index']) ?>">View All Products</a>
+                        <a class="collapse-item" href="<?= $this->Url->build(['controller'=>'Products','action' => 'add']) ?>">Add New Product</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Nav Item - Orders Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#collapseUtilities"
+                   aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-fw fa-wrench"></i>
+                    <span>Customers</span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                     data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Custom Utilities:</h6>
+                        <a class="collapse-item" href="utilities-border.html">Borders</a>
+                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                        <a class="collapse-item" href="utilities-other.html">Other</a>
+                    </div>
+                </div>
+            </li>
+
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
+        <main class="main">
+            <div class="container">
+                <?= $this->Flash->render() ?>
+                <?= $this->fetch('content') ?>
+            </div>
+        </main>
+    </div>
+    <!-- Bootstrap core JavaScript-->
+    <script src="/webroot/vendor/jquery/jquery.min.js"></script>
+    <script src="/webroot/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="/webroot/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="/webroot/js/sb-admin-2.min.js"></script>
+
+    <?= $this->fetch('script') ?>
 </body>
 </html>
+
+
+
