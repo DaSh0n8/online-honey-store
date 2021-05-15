7074eceaf53f1da1628e3683f0f693ba23139144
Brandon <blim0014@student.monash.edu>
2021-05-15 18:05:15 +1000

    Fixing css on navigation bar

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -161,9 +161,9 @@ $this->Html->css('min', ['block' => true]);
                 <div class="attr-nav">
                     <ul class="nav navbar-nav ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
                         <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                            <a href="#" id="userDropdown" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Account</span>
+                                <span>Account</span>
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
